2308425799ec9b0b55f52ed629141ef9d650d144
Bagaskara <karaeka6@gmail.com>
2022-10-14 13:04:13 +0700

    challenge#8 done

--- challenge#8.js
+++ challenge#8.js
@@ -1,26 +1,19 @@
 function pola(sentence) {
     //write your code here
     pengganti = "x"
-    pengganti2 = "x1"
+    pengganti2 = "y"
     angka = sentence.replace(/#/g, pengganti)
     angka2 = angka.replace(/[=*]/g, "")
     myArray = angka2.split("  ")
     newArray = myArray[2].replace(pengganti, pengganti2)
-    // myArray = myArray[2].replace('x', pengganti2)
-    myArray.splice(2,1,newArray)
-
-    for (let i = 0; i < myArray.length; i++) {
-        for (let j = 1; j < myArray.length; j++) {
-            myArray[i] = myArray[i].replace(pengganti,i)
-            let k = j - 2
-            myArray[j] = myArray[j].replace(pengganti2,k)
+    myArray.splice(2, 1, newArray)
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (myArray[0].replace(pengganti, i) * myArray[1] == myArray[2].replace(pengganti2, j)) {
+                return ([i,j]);
+            }
         }
-
     }
-    console.log(myArray)
 }
-
-// sentence = "4213 * 188 = 801204"
-// myArray = sentence.split(" ")
-// console.log(myArray);
 console.log(pola("42#3 * 188 = 80#204"))
+console.log(pola("8#61 * 895 = 78410#5"))
