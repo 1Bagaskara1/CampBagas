2767908be25791b0d235626ea2b4a67cf9be9ed5
Bagaskara <karaeka6@gmail.com>
2022-10-15 13:11:14 +0700

    task done

--- challenge#9.js
+++ challenge#9.js
@@ -1,39 +1,47 @@
 function spiral(param1) {
     //your code here
-    const inputNilai = param1;
-    let array = []
-    let arraySpiral = [];
-    let param2 = 0
- 
-    for (i = 0; i < param1 * param1; i++) {
-        // for (j = 0; j < param1; j++)
-        array.push(i)
+    let arrayPolosan = []
+    let matrix = []
+    let isi = -1;
+    let arrayHasil = [];
+    let barisAwal = 0;
+    let kolomAwal = 0;
+    let barisAkhir = param1 - 1;
+    let KolomAkhir = param1 - 1;
+
+    for (let i = 0; i < param1 * param1; i++) {
+        arrayPolosan.push(i)
     }
-    // return console.log(array)
-    for (j = 0; j < param1 - 1; j++) {
-        arraySpiral.push(array[j])
+    // return console.log(arrayPolosan);
+    for (let a = 0; a < param1; a++) {
+        matrix.push([])
+        for (let b = 0; b < param1; b++) {
+            matrix[a][b] = isi += +1;
+        }
     }
-    // return console.log(arraySpiral);
-    for (a = 0; a < param1 - 1; a++) {
-        param2 +=  array[param1]
-        arraySpiral.push(array[param2 - 1])
+    // return console.log(matrix);
+    while (kolomAwal <= KolomAkhir && barisAwal <= barisAkhir) {
+        for (let i = kolomAwal; i <= KolomAkhir; i++) {
+            arrayHasil.push(matrix[barisAwal][i])
+        }
+        barisAwal++
+        for (let i = barisAwal; i <= barisAkhir; i++) {
+            arrayHasil.push(matrix[i][KolomAkhir])
+        }
+        KolomAkhir--
+        for (let i = KolomAkhir; i >= kolomAwal; i--) {
+            arrayHasil.push(matrix[barisAkhir][i])
+        }
+        barisAkhir--
+        for (let i = barisAkhir; i >= barisAwal; i--) {
+            arrayHasil.push(matrix[i][kolomAwal])
+        }
+        kolomAwal++
     }
-    // return console.log(arraySpiral)
-    for (a = 1; a < param1; a++) {
-        arraySpiral.push(array[(param1*param1) - a])
-    }
-    // return console.log(arraySpiral)
-    // return console.log(array)
-    for (a = 1; a < param1 - 1; a++) {
-        arraySpiral.push(array[(param1 * (param1 - a))])
-    }
-    // return console.log(arraySpiral)
-    for (a = 0; a <= param1 - 2; a++) {
-        arraySpiral.push(array[(param1 + a)])
-    }
-    // return console.log(arraySpiral)
-    return console.log(arraySpiral)
+    
+    return console.log(arrayHasil)
 }
 
-(spiral(5));
-// (spiral(6));
+spiral(5);
+spiral(6);
+spiral(7);
