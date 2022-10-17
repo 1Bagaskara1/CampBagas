1e9c6d1a768829b0b4f52f7426550c6a5fac489a
Bagaskara <karaeka6@gmail.com>
2022-10-17 16:32:50 +0700

    challenge#12 updated

--- challenge#12.js
+++ challenge#12.js
@@ -5,48 +5,54 @@ let counterPertanyaan = 0;
 let counterSalah = 0;
 
 if (commandInputan == undefined) {
-    console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node challenge#12.js data.json atau node challenge#12.js math.json'")
-    process.exit(1);
+  console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node challenge#12.js data.json atau node challenge#12.js math.json'")
+  process.exit(1);
+}
+
+if (!fs.existsSync(commandInputan)) {
+  console.log(`Masukan file ${commandInputan} pada folder yang sama dengan challenge#12.js`);
+  process.exit(1);
 }
 
 let data = fs.readFileSync(`${commandInputan}`, 'utf8')
+
 let berkas = JSON.parse(data);
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: 'Jawaban: '
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Jawaban: '
 });
 
 if (commandInputan !== null) {
-    console.log(`\nSelamat datang di permainan Tebak-tebakan. Kamu akan diberikan pertanyaan dari file ini '${commandInputan}'. Untuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi.\n`)
-    listPertanyaan()
+  console.log(`\nSelamat datang di permainan Tebak-tebakan. Kamu akan diberikan pertanyaan dari file ini '${commandInputan}'. Untuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi.\n`)
+  listPertanyaan()
 }
 
 
 function listPertanyaan() {
-    if (counterPertanyaan < berkas.length) {
-        counterSalah = 0;
-        console.log(`\nPertanyaan: ${berkas[counterPertanyaan].definition}`)
-        rl.prompt();
-    } else {
-        console.log("Anda Berhasil!")
-        process.exit(1);
-    }
+  if (counterPertanyaan < berkas.length) {
+    counterSalah = 0;
+    console.log(`\nPertanyaan: ${berkas[counterPertanyaan].definition}`)
+    rl.prompt();
+  } else {
+    console.log("Anda Berhasil!")
+    process.exit(1);
+  }
 }
 
 rl.on('line', (line) => {
-    if (line == berkas[counterPertanyaan].term || line == berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line == berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
-        counterPertanyaan = counterPertanyaan + 1;
-        console.log('Anda Beruntung\n');
-        listPertanyaan()
-    } else if (line == 'skip') {
-        berkas.push(berkas[counterPertanyaan]);
-        counterPertanyaan += 1;
-        listPertanyaan()
-    } else if (line !== berkas[counterPertanyaan].term || line !== berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line !== berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
-        counterSalah += 1
-        console.log(`Anda kurang beruntung! anda telah salah ${counterSalah}, silahkan coba lagi.\n`);
-        rl.prompt();
-    }
+  if (line == berkas[counterPertanyaan].term || line == berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line == berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
+    counterPertanyaan = counterPertanyaan + 1;
+    console.log('Anda Beruntung\n');
+    listPertanyaan()
+  } else if (line == 'skip') {
+    berkas.push(berkas[counterPertanyaan]);
+    counterPertanyaan += 1;
+    listPertanyaan()
+  } else if (line !== berkas[counterPertanyaan].term || line !== berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line !== berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
+    counterSalah += 1
+    console.log(`Anda kurang beruntung! anda telah salah ${counterSalah}, silahkan coba lagi.\n`);
+    rl.prompt();
+  }
 });
