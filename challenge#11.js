687b548530e6a07c0eb2454b8db05a62bd4ec51b
Bagaskara <karaeka6@gmail.com>
2022-10-17 10:25:47 +0700

    challenge#11 updated

--- challenge#11.js
+++ challenge#11.js
@@ -7,52 +7,31 @@ console.log("Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban
 
 let counterPertanyaan = 0;
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: 'Tebakan: '
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Tebakan: '
 });
 
-async function Kuis() {
-
-    if (counterPertanyaan < berkas.length) {
-        console.log(`Pertanyaan: ${berkas[counterPertanyaan].definition}`);
-        rl.prompt();
-        rl.on('line', (line) => {
-            if (line == berkas[counterPertanyaan].term || line == berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line == berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
-                counterPertanyaan = counterPertanyaan + 1;
-                console.log('Selamat Anda Benar!\n');
-                Kuis();
-            } else {
-                console.log(`Wkwkwkwk, Anda kurang beruntung!\n`);
-                rl.prompt();
-            }
-
-        });
-
-    } else {
-        console.log("Hore Anda Menang!")
-        process.exit(1);
-    }
+function listPertanyaan() {
+  if (counterPertanyaan < berkas.length) {
+    console.log(`Pertanyaan: ${berkas[counterPertanyaan].definition}`)
+    rl.prompt();
+  } else {
+    console.log("Hore Anda Menang!")
+    process.exit(1);
+  }
 }
 
-Kuis()
+rl.on('line', (line) => {
+  if (line == berkas[counterPertanyaan].term || line == berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line == berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
+      counterPertanyaan = counterPertanyaan + 1;
+      console.log('Selamat Anda Benar!\n');
+      listPertanyaan()
+  } else {
+      console.log(`Wkwkwkwk, Anda kurang beruntung!\n`);
+      rl.prompt();
+  }
 
-                // switch (line.trim()) {
-                //     case (berkas[counterPertanyaan].term):
-                //         counterPertanyaan = counterPertanyaan + 1;
-                //         console.log('Selamat Anda Benar!\n');
-                //         Kuis();
-                //         break;
-                //     case (berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1)):
-                //         ccounterPertanyaan = counterPertanyaan + 1;
-                //         console.log('Selamat Anda Benar!\n');
-                //         Kuis();
-                //         break;
-                //     case (berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)):
-                //         counterPertanyaan = counterPertanyaan + 1;
-                //         console.log('Selamat Anda Benar!\n');
-                //         Kuis();
-                //         break;
-                //     default:
-                //         console.log(`Wkwkwkwk, Anda kurang beruntung!\n`);
-                //         // return 0;
+});
+
+listPertanyaan()
