1fffdaa98134b77bee339450960299ccd69f600c
Bagaskara <karaeka6@gmail.com>
2022-10-17 12:26:29 +0700

    challenge#12 updated

--- challenge#12.js
+++ challenge#12.js
@@ -2,7 +2,9 @@ const fs = require('fs')
 const readline = require('node:readline');
 const commandInputan = process.argv[2];
 let data = fs.readFileSync('data.json', 'utf8')
+let data2 = fs.readFileSync('math.json', 'utf8')
 let berkas = JSON.parse(data);
+let berkas2 = JSON.parse(data2);
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,13 +14,16 @@ const rl = readline.createInterface({
 
 let counterPertanyaan = 0;
 let counterSalah = 0;
-let arrayTampung = [];
 
-if (commandInputan !== "data.json") {
-  console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node challenge#12.js data.json'")
-  process.exit(1);
-} else {
+if (commandInputan == "data.json") {
   console.log(`\nSelamat datang di permainan Tebak-tebakan. Kamu akan diberikan pertanyaan dari file ini '${commandInputan}'. Untuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi.\n`)
+  listPertanyaan()
+} else if (commandInputan == "math.json") {
+  console.log(`\nSelamat datang di permainan Tebak-tebakan. Kamu akan diberikan pertanyaan dari file ini '${commandInputan}'. Untuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi.\n`)
+  listPertanyaanMtk()
+} else {
+  console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node challenge#12.js data.json atau node challenge#12.js math.json'")
+  process.exit(1);
 
 }
 
@@ -34,21 +39,47 @@ function listPertanyaan() {
 }
 
 rl.on('line', (line) => {
-  if (line == berkas[counterPertanyaan].term || line == berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line == berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
+  if (commandInputan == "data.json") {
+    if (line == berkas[counterPertanyaan].term || line == berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line == berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
       counterPertanyaan = counterPertanyaan + 1;
       console.log('Anda Beruntung\n');
       listPertanyaan()
+    } else if (line == 'skip') {
+      berkas.push(berkas[counterPertanyaan]);
+      counterPertanyaan += 1;
+      listPertanyaan()
+    } else if (line !== berkas[counterPertanyaan].term || line !== berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line !== berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
+      counterSalah += 1
+      console.log(`Anda kurang beruntung! anda telah salah ${counterSalah}, silahkan coba lagi.\n`);
+      rl.prompt();
+    }
+  } else if (commandInputan == "math.json") {
+      if (line == berkas2[counterPertanyaan].term || line == berkas2[counterPertanyaan].term.charAt(0).toUpperCase() + berkas2[counterPertanyaan].term.slice(1) || line == berkas2[counterPertanyaan].term.charAt(0).toLowerCase() + berkas2[counterPertanyaan].term.slice(1)) {
+      counterPertanyaan = counterPertanyaan + 1;
+      console.log('Anda Beruntung\n');
+      listPertanyaanMtk()
   } else if (line == 'skip') {
-    berkas.push(berkas[counterPertanyaan]);
+    berkas2.push(berkas2[counterPertanyaan]);
     counterPertanyaan += 1;
-    listPertanyaan()
-  } else if (line !== berkas[counterPertanyaan].term || line !== berkas[counterPertanyaan].term.charAt(0).toUpperCase() + berkas[counterPertanyaan].term.slice(1) || line !== berkas[counterPertanyaan].term.charAt(0).toLowerCase() + berkas[counterPertanyaan].term.slice(1)) {
+    listPertanyaanMtk()
+  } else if (line !== berkas2[counterPertanyaan].term || line !== berkas2[counterPertanyaan].term.charAt(0).toUpperCase() + berkas2[counterPertanyaan].term.slice(1) || line !== berkas2[counterPertanyaan].term.charAt(0).toLowerCase() + berkas2[counterPertanyaan].term.slice(1)) {
       counterSalah += 1
       console.log(`Anda kurang beruntung! anda telah salah ${counterSalah}, silahkan coba lagi.\n`);
       rl.prompt();
   }
+  }
 
 });
 
-listPertanyaan()
+function listPertanyaanMtk() {
+  if (counterPertanyaan < berkas2.length) {
+    counterSalah = 0;
+    console.log(`\nPertanyaan: ${berkas2[counterPertanyaan].definition}`)
+    rl.prompt();
+  } else {
+    console.log("Anda Berhasil!")
+    counterPertanyaan = 0
+    process.exit(1);
+  }
+}
 
