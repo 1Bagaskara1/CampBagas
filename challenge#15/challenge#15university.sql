a9701dead98a1cb504ef0cae938b81c156b34084
Bagaskara <karaeka6@gmail.com>
2022-10-20 20:50:22 +0700

    challenge#15 updated

--- challenge#15/challenge#15university.sql
+++ challenge#15/challenge#15university.sql
@@ -3,13 +3,13 @@
 
 select mahasiswa.*, jurusan.namajurusan from mahasiswa join jurusan on mahasiswa.idjurusan = jurusan.idjurusan;
 
-SELECT mahasiswa.*, jurusan.namajurusan FROM mahasiswa JOIN jurusan ON mahasiswa.idjurusan = jurusan.idjurusan GROUP BY NamaMahasiswa HAVING umur < 20;
+SELECT mahasiswa.*, jurusan.namajurusan FROM mahasiswa JOIN jurusan ON mahasiswa.idjurusan = jurusan.idjurusan WHERE umur < 20;
 
-SELECT mahasiswa.*, Kontrak.Nilai FROM Mahasiswa JOIN Kontrak ON Mahasiswa.NIM = Kontrak.NIM GROUP BY NamaMahasiswa HAVING Nilai <= 'B';
+SELECT mahasiswa.*, Kontrak.Nilai FROM Mahasiswa JOIN Kontrak ON Mahasiswa.NIM = Kontrak.NIM WHERE Nilai <= 'B';;
 
 SELECT Mahasiswa.NamaMahasiswa, Kontrak.NIM, SUM(SKS) AS Total_SKS FROM Mahasiswa JOIN Kontrak ON Mahasiswa.NIM = Kontrak.NIM GROUP BY KONTRAK.NIM HAVING Total_SKS > 10;
 
-SELECT mahasiswa.*, Mata_Kuliah.NamaMatkul FROM mahasiswa JOIN Mata_Kuliah ON mahasiswa.KodeMatkul = Mata_Kuliah.KodeMatkul GROUP BY NamaMahasiswa HAVING NamaMatkul = 'Data Mining';
+SELECT mahasiswa.*, Mata_Kuliah.NamaMatkul FROM mahasiswa JOIN Mata_Kuliah ON mahasiswa.KodeMatkul = Mata_Kuliah.KodeMatkul WHERE NamaMatkul = 'Data Mining';
 
 select Dosen.NamaDosen, (select count(*) from Kontrak WHERE Dosen.IdDosen = Kontrak.IdDosen) as total_mahasiswa from Dosen;
 
